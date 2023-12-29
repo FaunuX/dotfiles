98c2fac7fd500692f3a9f83f91f79851be91d083
FaunuX <judahpspiegel@gmail.com>
2023-12-29 15:38:37 -0800

    update

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -4,6 +4,6 @@ vim.opt.termguicolors = true
 vim.opt.virtualedit = 'block'
 
 if vim.g.neovide then
-	vim.o.guifont = "FiraCode Nerd Font, Regular:h6"
+	vim.o.guifont = "Fira Code:h6"
 	vim.g.neovide_transparency = 0.8
 end
